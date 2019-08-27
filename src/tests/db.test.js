78cdc9bdb48e30e5618e28078e3cc23ec210f69b
Sam Harris <sam@jam-bc.com>
2019-08-27 16:22:48 +0300

    basic test for different db queries

---
+++ src/tests/db.test.js
@@ -0,0 +1,67 @@
+const test = require("tape");
+const buildDatabase = require("../model/database/dbBuild");
+const queries = require("../model/queries/queries");
+
+test("tape test for db files is working", t => {
+  t.equal(1, 1, "1 equals 1");
+  t.end();
+});
+
+const log1 = {
+  id: 1,
+  username: 'Bobby',
+  meal_id: 5,
+  datetime: '2019-08-26 20:05:06',
+  portion_size: 7
+}
+
+const meal1 = {
+  id: 1,
+  title: 'Chicken Salad',
+  calories: 400,
+  ingredients: ['Chicken', 'Avocado', 'Lettuce', 'Onion']
+}
+
+test('Test that getAllLogs returns all the diary info', t => {
+  buildDatabase((err, res) => {
+    t.error(err, 'no errors whoop');
+    queries.getAllLogs((error, result) => {
+      if (error) console.log('we have an error with the query: ', error);
+      t.deepEqual(result[0], log1, 'returns the correct info from DB');
+      t.end;
+    })
+  })
+})
+
+test('Test that getUserLogs returns all the diary info for given user', t => {
+  buildDatabase((err, res) => {
+    t.error(err, 'no errors whoop');
+    queries.getUserLogs('Bobby', (error, result) => {
+      if (error) console.log('we have an error with the query: ', error);
+      t.deepEqual(result[0], log1, 'returns the correct info from DB');
+      t.end;
+    })
+  })
+})
+
+test('Test that getMeals returns all the meals in the database', t => {
+  buildDatabase((err, res) => {
+    t.error(err, 'no errors whoop');
+    queries.getMeals((error, result) => {
+      if (error) console.log('we have an error with the query: ', error);
+      t.deepEqual(result[0].title, meal1.title, 'returns the correct info from DB');
+      t.end;
+    })
+  })
+})
+
+test('Test that getMealById returns all info in a meal', t => {
+  buildDatabase((err, res) => {
+    t.error(err, 'no errors whoop');
+    queries.getMealById(1, (error, result) => {
+      if (error) console.log('we have an error with the query: ', error);
+      t.deepEqual(result[0], meal1, 'returns the correct info from DB');
+      t.end;
+    })
+  })
+})
