5d1a5ac9ed07a7b1ba6f3f5cf1fb629662e4e1a3
Sam Harris <sam@jam-bc.com>
2019-08-28 23:56:35 +0300

    removed commented code and neated formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const queries = require('../model/queries/queries.js');
+const queries = require('../model/queries/queries');
 
 const router = express.Router();
 
@@ -21,26 +21,6 @@ router.post('/logmeal', ({ body }, res) => {
   const id = parseInt(meal, 10);
 });
 
-// .then(res.render("home"))
-
-// router.post(
-//   "/facsters/:name/:location",
-//   ({ params: { name }, params: { location }, body }, res, next) => {
-//     console.log("Name", name);
-//     console.log("Location", location);
-//     queries
-//       .addLocation(name, location)
-//       // .then(userID => queries.getFacsterById(userID))
-//       // .then(console.log)
-//       .then(user => res.status(201).json(user))
-//       // .then("Then", console.log)
-//
-//       // .then(res.send("Hello Dexter"))
-//       // .then("201", console.log)
-//       .catch(err => next(err));
-//   }
-// );
-
 router.get('/submit', (req, res) => {
   res.render('meal-submission');
 });
@@ -52,9 +32,11 @@ router.post('/submit', ({ body }, res, next) => {
     .catch((err) => next(err));
 });
 
-router.get('/alllogs', (req, res, next) => queries
+router.get('/alllogs', (req, res, next) => {
+  queries
     .getAllUserLogs()
     .then((logs) => res.status(200).json(logs))
-    .catch((err) => next(err)),);
+    .catch((err) => next(err));
+});
 
 module.exports = router;
