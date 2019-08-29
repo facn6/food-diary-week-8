11de7cb18e3968e8d003c3a4771c44c746927f8d
אולגה מולינס <OLGA@wlghs-MacBook-Air.local>
2019-08-29 14:59:44 +0300

    changes

--- src/model/database/dbBuild.js
+++ src/model/database/dbBuild.js
@@ -1,21 +1,21 @@
 // const fs = require("fs");
-const {
-  QueryFile,
-} = require('pg-promise');
-const path = require('path');
-const connection = require('./dbConnection');
+const { QueryFile } = require("pg-promise");
+const path = require("path");
+const connection = require("./dbConnection");
 
-const sql = (file) => new QueryFile(path.join(__dirname, file), {
-  minify: true,
-});
+const sql = file =>
+  new QueryFile(path.join(__dirname, file), {
+    minify: true
+  });
 
-const build = sql('./db_build.sql');
+const build = sql("./db_build.sql");
 
-connection.query(build)
-  .then((res) => console.log('res', res))
-  .catch((e) => console.error('error', e));
+connection
+  .query(build)
+  .then(res => console.log("res", res))
+  .catch(e => console.error("error", e));
 
 module.exports = {
   build,
-  connection,
+  connection
 };
--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -1,155 +1,155 @@
-const tape = require('tape');
-const _test = require('tape-promise').default;
+const tape = require("tape");
+const _test = require("tape-promise").default;
 
 const test = _test(tape);
-const { build, connection } = require('../model/database/dbBuild');
-const queries = require('../model/queries/queries');
+const { build, connection } = require("../model/database/dbBuild");
+const queries = require("../model/queries/queries");
 
-test('tape test for db files is working', (t) => {
-  t.equal(1, 1, '1 equals 1');
+test("tape test for db files is working", t => {
+  t.equal(1, 1, "1 equals 1");
   t.end();
 });
 
 const log1 = {
   id: 1,
-  username: 'Bobby',
+  username: "Bobby",
   meal_id: 5,
-  datetime: '2019-08-26 20:05:06',
-  portion_size: 7,
+  datetime: "2019-08-26 20:05:06",
+  portion_size: 7
 };
 
 const newMealLog = {
-  username: 'Test',
-  meal: '2',
-  date: '2019-08-28',
-  time: '20:05:06',
-  portion: 1,
+  username: "Test",
+  meal: "2",
+  date: "2019-08-28",
+  time: "20:05:06",
+  portion: 1
 };
 
 const meal1 = {
   id: 1,
-  title: 'Chicken Salad',
+  title: "Chicken Salad",
   calories: 400,
-  ingredients: ['Chicken', 'Avocado', 'Lettuce', 'Onion'],
+  ingredients: ["Chicken", "Avocado", "Lettuce", "Onion"]
 };
 
 connection
   .query(build)
-  .then((res) => console.log('res', res))
-  .catch((e) => console.error('error', e));
+  .then(res => console.log("res", res))
+  .catch(e => console.error("error", e));
 
-test('Test that getAllLogs returns all the diary info', (t) => {
+test("Test that getAllLogs returns all the diary info", t => {
   queries
     .getAllLogs()
-    .then((result) => {
+    .then(result => {
       t.deepEqual(
         result[0].username,
         log1.username,
-        'returns the correct info from DB',
+        "returns the correct info from DB"
       );
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that getAllUserLogs returns all the diary info for given user', (t) => {
+test("Test that getAllUserLogs returns all the diary info for given user", t => {
   queries
-    .getAllUserLogs('Bobby')
-    .then((result) => {
+    .getAllUserLogs("Bobby")
+    .then(result => {
       t.deepEqual(
         result[0].username,
         log1.username,
-        'returns the correct info from DB',
+        "returns the correct info from DB"
       );
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that getSingleLog returns the correct meal log', (t) => {
+test("Test that getSingleLog returns the correct meal log", t => {
   queries
     .getSingleLog(1)
-    .then((result) => {
+    .then(result => {
       t.deepEqual(
         result[0].username,
         log1.username,
-        'returns the correct info from DB',
+        "returns the correct info from DB"
       );
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that getMeals returns all the meals in the database', (t) => {
+test("Test that getMeals returns all the meals in the database", t => {
   queries
     .getAllMeals()
-    .then((result) => {
+    .then(result => {
       t.deepEqual(
         result[0].title,
         meal1.title,
-        'returns the correct info from DB',
+        "returns the correct info from DB"
       );
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that getMealById returns all info in a meal', (t) => {
+test("Test that getMealById returns all info in a meal", t => {
   queries
     .getMealById(1)
-    .then((result) => {
-      t.deepEqual(result[0], meal1, 'returns the correct info from DB');
+    .then(result => {
+      t.deepEqual(result[0], meal1, "returns the correct info from DB");
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that getMealTitles returns all titles and ids', (t) => {
+test("Test that getMealTitles returns all titles and ids", t => {
   queries
     .getMealTitles(1)
-    .then((result) => {
+    .then(result => {
       t.deepEqual(
         result[0].id,
         meal1.id,
-        'returns the correct meal id info from DB',
+        "returns the correct meal id info from DB"
       );
       t.deepEqual(
         result[0].title,
         meal1.title,
-        'returns the correct meal title info from DB',
+        "returns the correct meal title info from DB"
       );
       t.end();
     })
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
 
-test('Test that logMeal posts a new meal to DB', (t) => {
+test("Test that logMeal posts a new meal to DB", t => {
   queries
     .logMeal(newMealLog)
     // .then((result) => console.log('post result = ', result));
     .then(
-      queries.getAllUserLogs('Test').then((result) => {
+      queries.getAllUserLogs("Test").then(result => {
         t.deepEqual(
           result[0].username,
           newMealLog.username,
-          'loads new meal log into db',
+          "loads new meal log into db"
         );
         t.end();
-      }),
+      })
     )
-    .catch((error) => {
-      if (error) console.log('we have an error with the query: ', error);
+    .catch(error => {
+      if (error) console.log("we have an error with the query: ", error);
     });
 });
