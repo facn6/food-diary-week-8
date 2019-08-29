f877bb68dd1793ec405bd707944f08889a3d9c05
Sam Harris <sam@jam-bc.com>
2019-08-29 14:56:36 +0300

    changed quotes to double"

--- src/model/database/dbConnection.js
+++ src/model/database/dbConnection.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
-const pgp = require('pg-promise')();
-const url = require('url');
+require("dotenv").config();
+const pgp = require("pg-promise")();
+const url = require("url");
 
 let { DATABASE_URL } = process.env;
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   DATABASE_URL = process.env.TEST_DB_URL;
 }
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('Environment variable DATABASE_URL must be set');
+  throw new Error("Environment variable DATABASE_URL must be set");
 }
 
 const params = url.parse(DATABASE_URL);
 
-const [username, password] = params.auth.split(':');
+const [username, password] = params.auth.split(":");
 
 const options = {
   host: params.hostname,
   port: params.port,
-  database: params.pathname.split('/')[1],
+  database: params.pathname.split("/")[1],
   max: process.env.DB_MAX_CONNECTION || 2,
   user: username,
   password,
-  ssl: params.hostname !== 'localhost',
+  ssl: params.hostname !== "localhost"
 };
 
 module.exports = pgp(options);
--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,52 @@
-const express = require('express');
+const express = require("express");
 
-const queries = require('../model/queries/queries.js');
+const queries = require("../model/queries/queries.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-router.get('/logmeal', (req, res, next) => {
+router.get("/logmeal", (req, res, next) => {
   queries
     .getMealTitles()
-    .then((getMealTitles) => res.render('logmeal', { getMealTitles }))
-    .catch((err) => next(err));
+    .then(getMealTitles => res.render("logmeal", { getMealTitles }))
+    .catch(err => next(err));
 });
 
-router.post('/logmeal', ({ body }, res, next) => {
+router.post("/logmeal", ({ body }, res, next) => {
   const { username } = body;
   queries
     .logMeal(body)
     .then(res.redirect(`/logs/${username}`))
-    .catch((err) => next(err));
+    .catch(err => next(err));
 });
 
-router.get('/logs/:username', (req, res, next) => {
+router.get("/logs/:username", (req, res, next) => {
   const name = req.params.username;
   queries
     .getAllUserLogs(name)
-    .then((userLogs) => res.render('logs', { userLogs }))
-    .catch((err) => next(err));
+    .then(userLogs => res.render("logs", { userLogs }))
+    .catch(err => next(err));
 });
 
-router.get('/submit', (req, res) => {
-  res.render('meal-submission');
+router.get("/submit", (req, res) => {
+  res.render("meal-submission");
 });
 
-router.post('/submit', ({ body }, res, next) => {
+router.post("/submit", ({ body }, res, next) => {
   queries
     .addMeal(body)
-    .then(res.redirect('/'))
-    .catch((err) => next(err));
+    .then(res.redirect("/"))
+    .catch(err => next(err));
 });
 
-router.get('/alllogs', (req, res, next) => queries
+router.get("/alllogs", (req, res, next) =>
+  queries
     .getAllLogs()
-    .then((logs) => res.status(200).json(logs))
-    .catch((err) => next(err)),);
+    .then(logs => res.status(200).json(logs))
+    .catch(err => next(err))
+);
 
 module.exports = router;
