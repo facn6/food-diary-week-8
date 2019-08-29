5bc1140e0e35122711513ba72b47f4001fbc2f30
אולגה מולינס <OLGA@wlghs-MacBook-Air.local>
2019-08-29 14:11:40 +0300

    the user can now see meal details on a new page

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,68 @@
-const express = require('express');
+const express = require("express");
 
-const queries = require('../model/queries/queries.js');
+const queries = require("../model/queries/queries.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-router.get('/logmeal', (req, res, next) => {
+router.get("/logmeal", (req, res, next) => {
   queries
     .getMealTitles()
-    .then((getMealTitles) => res.render('logmeal', { getMealTitles }))
-    .catch((err) => next(err));
+    .then(getMealTitles => res.render("logmeal", { getMealTitles }))
+    .catch(err => next(err));
 });
 
-router.post('/logmeal', ({ body }, res, next) => {
+router.post("/logmeal", ({ body }, res, next) => {
   const { username } = body;
   queries
     .logMeal(body)
     .then(res.redirect(`/logs/${username}`))
-    .catch((err) => next(err));
+    .catch(err => next(err));
 });
 
-router.get('/logs/:username', (req, res, next) => {
+router.get("/logs/:username", (req, res, next) => {
   const name = req.params.username;
   queries
     .getAllUserLogs(name)
-    .then((userLogs) => res.render('logs', { userLogs }))
-    .catch((err) => next(err));
+    .then(userLogs => res.render("logs", { userLogs }))
+    .catch(err => next(err));
 });
 
-router.get('/submit', (req, res) => {
-  res.render('meal-submission');
+router.get(
+  "/logs/mealdetails/:title/:calories/:ingredients/:portion_size",
+  (req, res) => {
+    console.log("Request params:", req.params);
+    const { title, calories, ingredients, portion_size } = req.params;
+    const totalCal = calories * portion_size;
+    res.render("onemealinfo", {
+      title,
+      calories,
+      ingredients,
+      portion_size,
+      totalCal
+    });
+  }
+);
+
+router.get("/submit", (req, res) => {
+  res.render("meal-submission");
 });
 
-router.post('/submit', ({ body }, res, next) => {
+router.post("/submit", ({ body }, res, next) => {
   queries
     .addMeal(body)
-    .then(res.render('home'))
-    .catch((err) => next(err));
+    .then(res.render("home"))
+    .catch(err => next(err));
 });
 
-router.get('/alllogs', (req, res, next) => queries
+router.get("/alllogs", (req, res, next) =>
+  queries
     .getAllLogs()
-    .then((logs) => res.status(200).json(logs))
-    .catch((err) => next(err)),);
+    .then(logs => res.status(200).json(logs))
+    .catch(err => next(err))
+);
 
 module.exports = router;
